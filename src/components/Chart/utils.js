5d4b8c48b21cb4d20b874cb0724de30d210575a8
lijie33402 <823997187@qq.com>
2021-12-13 21:58:03 +0800

    feature: 合并监控宽高hook中的state，优化更新

--- src/components/Chart/utils.js
+++ src/components/Chart/utils.js
@@ -17,6 +17,7 @@ export const combineChartDimensions = dimensions => {
   }
 }
 
+// 获取容器宽高，如果没有传入width和height则监控容器变化更新width和height，如果传入了则直接使用传入宽高。
 export const useChartDimensions = passedSettings => {
   const ref = useRef()
   const dimensions = combineChartDimensions(passedSettings)
@@ -24,8 +25,10 @@ export const useChartDimensions = passedSettings => {
   // if (dimensions.width && dimensions.height) return [ref, sdimensions]
   const initialWidth = dimensions.width || 0;
   const initialHeight = dimensions.height || 0;
-  const [width, changeWidth] = useState(initialWidth)
-  const [height, changeHeight] = useState(initialHeight)
+  // const [width, changeWidth] = useState(initialWidth)
+  // const [height, changeHeight] = useState(initialHeight)
+  // 合并有关联的state，减少组件更新
+  const [wd, changeWd] = useState({width: initialWidth, height: initialHeight});
 
   useEffect(() => {
     const element = ref.current
@@ -35,8 +38,14 @@ export const useChartDimensions = passedSettings => {
 
       const entry = entries[0]
 
-      if (width !== entry.contentRect.width) changeWidth(entry.contentRect.width)
-      if (height !== entry.contentRect.height) changeHeight(entry.contentRect.height)
+      // if (width !== entry.contentRect.width) changeWidth(entry.contentRect.width)
+      // if (height !== entry.contentRect.height) changeHeight(entry.contentRect.height)
+      if (wd.width !== entry.contentRect.width || wd.height !== entry.contentRect.height) {
+        changeWd({
+          width: entry.contentRect.width,
+          height: entry.contentRect.height
+        })
+      }
     })
 
     resizeObserver.observe(element)
@@ -46,8 +55,8 @@ export const useChartDimensions = passedSettings => {
 
   const newSettings = combineChartDimensions({
     ...dimensions,
-    width: dimensions.width || width,
-    height: dimensions.height || height,
+    width: dimensions.width || wd.width,
+    height: dimensions.height || wd.height,
   })
 
   return [ref, newSettings]
